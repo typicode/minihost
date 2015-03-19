fc9dd9106397ca6de2951fe9f56fb703e43bbbf2
Typicode <typicode@gmail.com>
2015-03-19 05:14:17 +0100

    Add --stop

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ var pkg = require('../package.json')
 
 updateNotifier({pkg: pkg}).notify()
 
-var argv = yargs
+var yargs = yargs
   .usage('$0 [opts] -- <command>')
   .help('help').alias('help', 'h')
   .version(pkg.version, 'version').alias('version', 'v')
@@ -16,13 +16,26 @@ var argv = yargs
       describe: 'Set server name'
     }
   })
+  .options({
+    stop: {
+      describe: 'Stop minihost'
+    }
+  })
   .example('~/express$ $0 nodemon', 'http://express.127.0.0.1.xip.io:2000')
   .example('~/front$ $0 gulp server', 'http://front.127.0.0.1.xip.io:2000')
   .example('~/other$ $0 -n app -- nodemon', 'http://app.127.0.0.1.xip.io:2000')
   .epilog('To list running servers, go to http://localhost:2000')
-  .demand(1)
-  .argv
 
-h(argv, function (err) {
+var argv = yargs.argv
+
+// --stop
+if (argv.stop) return h.stop(function (err) {
+  if (err) return console.error('minihost is not running')
+  console.log('Successfully stopped minihost')
+})
+
+// [opts] -- <command>
+if (argv._.length === 0) return yargs.showHelp()
+h.run(argv, function (err) {
   if (err) throw err
 })
